dcacfed4a7e246b0e5f095dda8955816de3f9f79
Akash Ajay Malvi <akashmalvi@gmail.com>
2025-12-07 15:52:12 +0530

    commit-3

--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -5,7 +5,7 @@ function Sidebar() {
   return (
     <div className='w-[18%] min-h-screen border-r-2 border-gray-200'>
         <div className='flex flex-col gap-4 pt-6 pl-[20%] text-[15px]'>
-            <NavLink className="flex items-center gap-3 border border-gray-300 border-r-0 px-3 py-2 rounded-"  to={"/add"}>
+            <NavLink className="flex items-center gap-3 border border-gray-300 border-r-0 px-3 py-2 rounded-"  to={"/"}>
             <img className='w-5 h-5' src={assets.add_icon} alt="" />
             <p className='hidden sm:block'>Add Items</p>
             </NavLink>
--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,21 +1,26 @@
-import React from 'react'
-import { assets } from '../assets/frontend_assets/assets'
+import React, { useContext } from 'react'
+
+import {shopContext} from "../context/ShopContext"
+import { Link } from 'react-router-dom'
 
 const Footer = () => {
+    const {navigate} = useContext(shopContext)
   return (
     < >
         <div className='flex  flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-10 my-10 mt-40 text-sm '>
         <div>
-            <img src={assets.logo} className='mb-5 w-32' alt="" />
-            <p className='w-full md:w-1/2 text-gray-600 '>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestias vel ab, eum dolor facere perspiciatis illo, cupiditate iusto magnam nam asperiores aliquid, at dignissimos vitae rerum ipsam quisquam laborum perferendis?</p>
+            <Link to={"/"}>
+<h1 onClick={()=>navigate("/")} className="text-3xl ">ShopEase</h1>
+</Link>
+            <p className='w-full md:w-1/2 text-gray-600 '>Shopease brings you a smooth and reliable online shopping experience with quality products at honest prices. Our mission is to make shopping simple, fast, and secure for everyone.</p>
         </div>
 <div >
     <p className="text-xl font-medium mb-5">COMPANY</p>
     <ul className="flex flex-col gap-1 text-gray-600 cursor-pointer ">
-        <li className='hover:text-gray-400'>HOME</li>
-        <li className='hover:text-gray-400'>ABOUT US</li>
-        <li className='hover:text-gray-400'>DELIVERY</li>
-        <li className='hover:text-gray-400'>PRIVACY POLICY</li>
+        <Link to='/' className='hover:text-gray-400'>HOME</Link>
+        <Link to='/about' className='hover:text-gray-400'>ABOUT US</Link>
+        <Link to='/orders' className='hover:text-gray-400'>DELIVERY</Link>
+        <Link to='/' className='hover:text-gray-400'>PRIVACY POLICY</Link>
     </ul>
 </div>
 <div>
@@ -28,7 +33,7 @@ const Footer = () => {
         </div>
         <div className="text-base">
             <hr className='text-gray-200' />
-            <p className='py-5  text-center'>Copyright 2025 @ forever.com ,All rights are Reserved.</p>
+            <p className='py-5  text-center'>Copyright 2025 @ ShopEase.com ,All rights are Reserved.</p>
         </div>
         </>
   )
