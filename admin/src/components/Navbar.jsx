55d64b989a0c6b103e1d13fc8e97ad02e8c2c732
Akash Ajay Malvi <akashmalvi@gmail.com>
2025-12-24 08:59:43 +0530

    admin

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -6,7 +6,8 @@ function Navbar({setToken}) {
   return (
     <div className='flex items-center py-2 px-[4%] justify-between'>
    
-<h1 className="text-3xl text-red-300">ShopEase</h1>
+<h1 className="text-3xl text-black">WebCart
+</h1>
 
       <button onClick={()=>setToken('')} className='bg-gray-600 text-white px-5 py-2  sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm cursor-pointer '>Logout</button>
       
